2573cd3d4740dc22cc5356f71d4d44e77ba72975
Cyrus Rice <cyrusric@amazon.com>
2021-02-23 22:02:36 -0800

    Added movement logic

--- static/chess_frontend.js
+++ static/chess_frontend.js
@@ -1,28 +1,12 @@
 // Decline player move if space is occupied by one of their pieces
 function onDrop (source, target, piece, newPos, oldPos, orientation) {
-  // Need to check for valid move, meaning 
-  // - It's your turn (b/w)
-  // - You're moving on a valid square
-  //   (a) Can't move on own piece (b/w)
-  //   (b) Limited by type of piece (k/q/p/n/r/b)
-  //   (c) Can't make move if it results in checkmate
-  /*console.log('piece: ' + piece)
-  console.log('source: ' + source)
-  console.log('target: ' + target)
-  console.log('newPos: ' + newPos)
-  console.log('oldPos: ' + oldPos)
-  console.log('orientation: ' + orientation)*/
-  /*if(board.position()[target] === undefined)
-    return
-  if ((piece.search(/b/) !== -1) && (board.position()[target].search(/b/) !== -1) ||
-  (piece.search(/w/) !== -1) && (board.position()[target].search(/w/) !== -1)) {
-    return 'snapback'
-  }*/
-  /*if(chess.validate_fen(new Chess(Chessboard.objToFen(newPos)).fen()).valid === true) {
-    console.log('good!');
-  } else {
-    console.log('bad');
-  }*/
+  // Need to check for valid move
+  const obj = {from : source, to : target};
+  var moveSuccess = chess.move(obj);
+  if (moveSuccess === null) 
+    return 'snapback';
+  else 
+    return;
 }
 
 
