5b954bb8de4086da8679edad8862c584c02b4277
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2021-03-04 19:15:32 -0800

    updated disconnect logic to close only one socket and added logout

--- app.py
+++ app.py
@@ -1,34 +1,46 @@
-from flask import Flask, render_template, url_for, request,redirect,flash
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_socketio import SocketIO, emit
 import bcrypt
 import chess
 from models import *
+import copy
 
 async_mode = None
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app,log_output=True,logger=True,async_mode=async_mode)
+socketio = SocketIO(app, log_output=True, logger=True, async_mode=async_mode)
 clients = dict()
+
+
 @app.route('/')
 def home():
     # app.route('/')
     return render_template("home.html", sync_mode=socketio.async_mode)
 
+
 @app.route('/about')
 def about():
-    return render_template("about.html",sync_mode=socketio.async_mode)
+    return render_template("about.html", sync_mode=socketio.async_mode)
+
 
 @app.route('/signup')
 def signup():
-    return render_template("signup.html",sync_mode=socketio.async_mode)
+    return render_template("signup.html", sync_mode=socketio.async_mode)
+
 
 @app.route('/account/<userid>')
 def account(userid):
-    return render_template("account.html",userid=userid,sync_mode=socketio.async_mode)
+    return render_template("account.html", userid=userid, sync_mode=socketio.async_mode)
 
-@app.route('/signin', methods = ['POST'])
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('home'))
+
+
+@app.route('/signin', methods=['POST'])
 def signin():
     if request.method == 'POST':
         userid = request.form['userid']
@@ -36,18 +48,18 @@ def signin():
         useridCount = db['users'].count_documents({'userid': userid})
         if useridCount > 0:
             user = db['users'].find({'userid': userid})[0]
-            if bcrypt.checkpw(password,user['password']):
+            if bcrypt.checkpw(password, user['password']):
                 print(request.args)
-                return redirect(url_for('account',userid=userid))
-        return render_template("home.html",sync_mode=socketio.async_mode)
+                return redirect(url_for('account', userid=userid))
+        return render_template("home.html", sync_mode=socketio.async_mode)
 
 
-@app.route('/createAccount', methods = ['POST'])
+@app.route('/createAccount', methods=['POST'])
 def createAccount():
     if request.method == 'POST':
         newuser = dict()
-        newuser['firstname'] = request.form['firstname'] 
-        newuser['lastname'] = request.form['lastname'] 
+        newuser['firstname'] = request.form['firstname']
+        newuser['lastname'] = request.form['lastname']
         newuser['email'] = request.form['email']
 
         userid = request.form['userid']
@@ -55,7 +67,7 @@ def createAccount():
 
         password = request.form['password'].encode('utf-8')
         salt = bcrypt.gensalt()
-        pass_hash = bcrypt.hashpw(password,salt)
+        pass_hash = bcrypt.hashpw(password, salt)
         newuser['password'] = pass_hash
 
         newuser['friends'] = {}
@@ -64,7 +76,8 @@ def createAccount():
         newuser['stats'] = {}
 
         useridCount = db['users'].count_documents({'userid': userid})
-        emailCount = db['users'].count_documents({'email': request.form['email']})
+        emailCount = db['users'].count_documents(
+            {'email': request.form['email']})
 
         if useridCount > 0:
             flash('userid already exists, pick a different one')
@@ -74,25 +87,27 @@ def createAccount():
 
         else:
             db['users'].insert_one(newuser)
-            return redirect(url_for('account',userid=userid))
+            return redirect(url_for('account', userid=userid))
 
         return redirect(url_for('signup'))
+
 
 @app.route('/forgotPassword')
 def forgotPassword():
     return render_template("forgotPassword.html", sync_mode=socketio.async_mode)
 
+
 @socketio.on('connected')
 def connected(message):
     clients[request.sid] = message['userid']
     try:
-        userSockets = clients[message['userid']]
+        userSockets = copy.deepcopy(clients[message['userid']])
     except:
         userSockets = list()
-    
+
     userSockets.append(request.sid)
     clients[message['userid']] = userSockets
-    print(clients)
+
 
 @socketio.on('disconnect')
 def disconnect():
@@ -103,19 +118,20 @@ def disconnect():
         userOnSocket = None
 
     if userOnSocket:
-        userSockets = clients[userOnSocket]
-        for userSocket in userSockets:
-            if userSocket in clients.keys():
-                clients.pop(userSocket)
-        clients.pop(userOnSocket)
-        print(clients)
-       
+        userSockets = copy.deepcopy(clients[userOnSocket])
+        if request.sid in userSockets:
+            userSockets.remove(request.sid)
+        if len(userSockets) == 0:
+            clients.pop(userOnSocket)
+        else:
+            clients[userOnSocket] = userSockets
+
+
 @socketio.on('update board')
 def broadcastFen(message):
     emit('broadcast fen', {'fen': message['fen']}, broadcast=True)
 
 
-
 if __name__ == "__main__":
-    socketio.run(app,debug=True)
-    #app.run(debug=True)
+    socketio.run(app, debug=True)
+    # app.run(debug=True)
--- static/js/account.js
+++ static/js/account.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    activateTab('FriendsContent');
-  });
-  
-  function activateTab(tab){
-    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
-  };
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+
+window.onload = function () {
+  home.innerHTML = '';
+  about.innerHTML = '';
+};
