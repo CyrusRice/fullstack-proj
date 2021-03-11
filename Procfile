80df032b9d0d36de917752e6423ea2e0f4a09089
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2021-03-10 20:45:56 -0800

    more experiments for gevent in procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app -k gevent
+web: gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 -b 127.0.0.1:5000 app:app
--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import bcrypt
 
-import gevent
+from gevent import monkey
+monkey.patch_all()
+
 from common import *
 import copy
 from bson.json_util import dumps
