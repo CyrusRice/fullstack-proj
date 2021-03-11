3bbc6ac23097cc52536bcf0a752f79e67c206be5
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2021-03-10 16:53:39 -0800

    fixed files for Heroku. Rev3

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import bcrypt
-import chess
 #from models import *
 from common import *
 import copy
