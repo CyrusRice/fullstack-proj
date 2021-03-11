0a3433451ea74b1e1f1b9f0c23c8c2af344ffb9e
VinodhKotipalli <50426917+VinodhKotipalli@users.noreply.github.com>
2021-03-10 20:27:44 -0800

    added gevent in app.py to try addressing heroku slowness

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import bcrypt
-#from models import *
+
+import gevent
 from common import *
 import copy
 from bson.json_util import dumps
